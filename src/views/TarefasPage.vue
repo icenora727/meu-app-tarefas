8e22881f63082a6f13f81dbe1f4945ddf5f76c54
cenora <inacioviniciusgabriel@gmail.com>
2026-04-07 14:49:15 -0300

    refactor: input por IonInput com validação + validação no button

--- src/views/TarefasPage.vue
+++ src/views/TarefasPage.vue
@@ -9,7 +9,16 @@
             </IonToolbar>
         </IonHeader>
         <IonContent>
-            <IonInput v-model="novaTarefa" placeholder="Digite uma Tarefa" label="Nome da Tarefa" labelPlacement="floating"/>
+            <IonInput 
+            v-model="novaTarefa"
+            placeholder="Digite uma Tarefa" 
+            label="Nome da Tarefa" 
+            labelPlacement="floating" 
+            :clear-input="true"
+            :errorText="erroTarefa"
+            :class="{'ion-invalid ion-touched': tocado && erroTarefa}"
+            @ionBlur="tocado = true"
+            />
 
             <IonButton expand="block" @click="adicionarTarefa" fill="solid" color="primary">
                 <IonIcon :icon="addOutline"></IonIcon>
@@ -36,21 +45,28 @@
 import { IonBackButton, IonButton, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
 import { trashOutline, addOutline } from 'ionicons/icons';
 
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const tarefas = ref<string[]>([])
 const novaTarefa = ref('')
 
 const adicionarTarefa = () => {
+    tocado.value = true
+    if (erroTarefa.value) return
     if (novaTarefa.value.trim() === '') return
 
     tarefas.value.push(novaTarefa.value)
     novaTarefa.value = ''
+    tocado.value = false
 }
 
 const removerTarefa = (index: number) => {
     tarefas.value.splice(index, 1)
 }
+
+const erroTarefa = computed( () => 
+    !novaTarefa.value.trim() ? "Campo Obrigatório" : "")
+const tocado = ref(false)
 </script>
 
 
