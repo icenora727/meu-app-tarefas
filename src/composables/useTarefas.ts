b374354791d23179d61bc56c4c77e0814d85638b
Vinícius Gabriel Inacio <164924053+Inacio727@users.noreply.github.com>
2026-04-27 19:47:49 -0300

    Update useTarefas.ts

--- src/composables/useTarefas.ts
+++ src/composables/useTarefas.ts
@@ -8,29 +8,61 @@ interface Tarefa {
 
 export function useTarefas() {
   const tarefas = ref<Tarefa[]>([]);
-  const novaTarefa = ref("");
   const busca = ref('')
   const filtroAtivo = ref<'todas' | 'pendentes' | 'feitas'>('todas')
+  //computed: filtra por texto de busca E pelo filtro ativo
+  const filtradas = computed( () => {
+    const termo = busca.value.toLowerCase()
+    return tarefas.value
+    .filter(t => t.texto.toLowerCase().includes(termo))
+    .filter(t => {
+      if (filtroAtivo.value === 'pendentes') return !t.feita
+      if (filtroAtivo.value === 'feitas') return t.feita
+    })
+  })
 
-  const adicionarTarefa = () => {
-    tocado.value = true;
-    if (erroTarefa.value) return;
-    if (novaTarefa.value.trim() === "") return;
+  //computed: total de pendentes
+  const totalPendentes = computed(
+    () => tarefas.value.filter(t => !t.feita).length
+  )
 
-    tarefas.value.push(novaTarefa.value);
-    novaTarefa.value = "";
-    tocado.value = false;
-  };
+  function adicionar(texto: string) {
+    if (!texto.trim()) return
+    tarefas.value.push({ id: Date.now(), texto, feita: false})
+  }
 
-  const removerTarefa = (index: number) => {
-    tarefas.value.splice(index, 1);
-  };
+  function remover(id: number) {
+    tarefas.value = tarefas.value.filter(t => t.id !== id)
+  }
 
-  const erroTarefa = computed(() =>
-    !novaTarefa.value.trim() ? "Campo Obrigatório" : "",
-  );
-  const tocado = ref(false);
+  function concluir(id: number) {
+    const t = tarefas.value.find(t => t.id === id)
+    if (t) t.feita = !t.feita
+  }
+
+  return { tarefas, busca, filtroAtivo, filtradas, totalPendentes, adicionar, remover, concluir}
 }
+
+
+  // const adicionarTarefa = () => {
+  //   tocado.value = true;
+  //   if (erroTarefa.value) return;
+  //   if (novaTarefa.value.trim() === "") return;
+
+  //   tarefas.value.push(novaTarefa.value);
+  //   novaTarefa.value = "";
+  //   tocado.value = false;
+  // };
+
+//   const removerTarefa = (index: number) => {
+//     tarefas.value.splice(index, 1);
+//   };
+
+//   const erroTarefa = computed(() =>
+//     !novaTarefa.value.trim() ? "Campo Obrigatório" : "",
+//   );
+//   const tocado = ref(false);
+// }
 
 
 // import { ref, computed } from "vue";
